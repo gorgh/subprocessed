c2c29b17c352b7d47e92ed3b8485562f8ef435d0
Yohann Pipeau <yohann.pipeau@gmail.com>
2024-10-24 17:46:49 +0200

    Generate the HTML documentation

---
+++ doc/source/conf.py
@@ -0,0 +1,12 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# Project information
+project = "subprocessing"
+
+# General configuration
+extensions = [
+    "sphinx_mdinclude",
+]
